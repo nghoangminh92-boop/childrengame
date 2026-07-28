d8b1a2f97af469ae92a5b736bbf82887738dc9f4
hoangminh <nghoangminh92@gmail.com>
2026-07-29 08:43:04 +0900

    42

--- src/pages/game/quiz/GamePlay.jsx
+++ src/pages/game/quiz/GamePlay.jsx
@@ -6,7 +6,7 @@ import { useAuth } from "../../../context/AuthContext.jsx";
 import QuestionCard from "../../../components/QuestionCard.jsx";
 import HeartsDisplay from "../../../components/HeartsDisplay.jsx";
 import ProgressBar from "../../../components/ProgressBar.jsx";
-import "../style/GamePlay.css";
+import "../../../style/GamePlay.css";
 
 const MAX_LIVES = 3;
 const POINTS_PER_CORRECT = 10;
